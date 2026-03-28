96e26074b27e345d819fd299ed7436a9e1163e93
Evgenii Ostapchenko <jena3573467@gmail.com>
2026-03-28 09:22:38 +0600

    feat: add backend swagger

--- users_api/alembic/env.py
+++ users_api/alembic/env.py
@@ -84,4 +84,4 @@ async def run_migrations_online() -> None:
 if context.is_offline_mode():
     run_migrations_offline()
 else:
-    run_migrations_online()
+    asyncio.run(run_migrations_online())
